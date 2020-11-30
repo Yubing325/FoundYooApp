0f7a0989dde8a31f9469884fef8791af15cd979d
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-11-30 08:07:20 -0600

    add authentication middleware

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,10 +18,22 @@ namespace API.Controllers
 
         }
 
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();            
+        }
+        
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null) return BadRequest("this user doesn't exisit");
+
+            return user;
         }
     }
 }
