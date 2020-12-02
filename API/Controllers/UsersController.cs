10b6a5cfeb8ec5795af56ab00a6ce196f822f357
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-12-02 10:36:36 -0600

    add seeding

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -31,16 +31,16 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            var user = await _userRepository.GetUserByIdAsync(id);
+        // [Authorize]
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<AppUser>> GetUser(int id)
+        // {
+        //     var user = await _userRepository.GetUserByIdAsync(id);
 
-            if (user == null) return BadRequest("this user doesn't exisit");
+        //     if (user == null) return BadRequest("this user doesn't exisit");
 
-            return user;
-        }
+        //     return user;
+        // }
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
