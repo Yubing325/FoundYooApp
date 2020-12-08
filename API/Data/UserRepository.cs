be871497bcfa439daf7bc5c6ceb7bbe807b6c462
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-12-08 07:19:14 -0600

    add age filters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -33,11 +34,22 @@ namespace API.Data
 
         public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            var query = _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                        .AsNoTracking();
+            var query = _context.Users.AsQueryable();
+            //exclude self
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            //filter gender
+            query = query.Where(u => u.Gender == userParams.Gender);
+            //filter ages
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+            
 
             return await 
-                PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber,userParams.PageSize);
+                PagedList<MemberDto>.CreateAsync(query
+                                    .ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking(), 
+                                    userParams.PageNumber,userParams.PageSize);
 
             //  return await _context.Users 
             //     .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
